8ef9fb499d95bf299673c354637495e91cd4f5e0
Shourya Saxena <saxenashourya000@gmail.com>
2024-10-16 14:15:04 +0530

    Add organization settings route for configuration and management

---
+++ app/(platform)/(dashboard)/organization/[orgId]/settings/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { OrganizationProfile } from '@clerk/nextjs';
+
+const SettingsPage = () => {
+  return (
+    <div className="w-full">
+        <OrganizationProfile 
+            appearance={{
+                elements: {
+                    rootBox: {
+                        boxShadow: "none",
+                        width: "100%"
+                    },
+                    cardBox: {
+                        boxShadow: "none",
+                        width: "100%",
+                        border: "1px solid #e5e7eb"
+                    }
+                }
+            }}
+            afterLeaveOrganizationUrl='/create-org'
+            routing="hash"
+        />
+    </div>
+  );
+};
+
+export default SettingsPage;
